b117bbdd12271ea7e1fdf14ba88e00c8c1e75124
Mikkel2833 <Mikkel2833@gmail.com>
2015-01-15 19:41:24 +0100

    update

---
+++ config.php
@@ -0,0 +1,4 @@
+$servername = "94.231.108.31";
+$username = "bylang_dk";
+$password = "C3pSvbGEBqEt2pGwpey8";
+$dbname = "bylang_dk_db";
--- sqlconnection.php
+++ sqlconnection.php
@@ -1,8 +1,5 @@
 <?php
-$servername = "94.231.108.31";
-$username = "bylang_dk";
-$password = "C3pSvbGEBqEt2pGwpey8";
-$dbname = "bylang_dk_db";
+include 'config.php';
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
